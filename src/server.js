05ed2fee6600038268acd1cf23c92331dcac7bec
Dominic Coelho <domwork@live.com>
2018-08-01 16:09:39 +0100

    basic server setup

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+// const router = require("./routes")
+const port = process.env.PORT || 4000;
+const host = process.env.HOSTNAME || "localhost";
+
+const server = http.createServer();
+
+server.listen(port, host, () => {
+    console.log(`server running on ${host} at ${port}`)
+})

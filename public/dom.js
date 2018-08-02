666941667dc416b9c4fae6bff52ba31f7c5934da
martingaston <squidman@gmail.com>
2018-08-02 15:09:47 +0100

    fix API paths

--- public/dom.js
+++ public/dom.js
@@ -6,7 +6,7 @@ geoLoc.addEventListener('click', (event) => {
   event.preventDefault();
   console.log('button works?');
   geoLocate((latitude, longitude) => {
-    const locString = `/?lat=${latitude}&long=${longitude}`;
+    const locString = `/coords/?lat=${latitude}&long=${longitude}`;
     xhr('GET', locString, (geolocationData) => {
       // ADD GEOLOCATION DISPLAY CODE HERE :D
       console.log(geolocationData);
@@ -18,7 +18,7 @@ postcodeLoc.addEventListener('click', (event) => {
   event.preventDefault();
   console.log('Postcode Search Button Pressed!');
   const postcode = input.value;
-  const postcodeString = `/?postcode=${encodeURI(postcode.replace(' ', ''))}`;
+  const postcodeString = `/postcode/?postcode=${encodeURI(postcode.replace(' ', ''))}`;
   console.log(postcodeString);
   xhr('GET', postcodeString, (postcodeData) => {
     // ADD POSTCODE FORMATTING/DISPLAY CODE HERE :D

c6faaafdbac7cefdb50855c7e2817dfe55b3ec2f
Dominic Coelho <domwork@live.com>
2018-08-02 11:31:07 +0100

    added writeHead

--- src/handlers.js
+++ src/handlers.js
@@ -1,32 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const mime = require('mime-types');
-const buildPath = function(myPath){
-    return path.join(__dirname, "..", 'public', myPath);
-}
-
-
-function mimeGenerator(request, response, url){
-  const lookup = mime.lookup(url)
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime-types");
+const buildPath = function(myPath) {
+  return path.join(__dirname, "..", "public", myPath);
 };
 
 
 const handler = {
-    home: function (request, response){
-        console.log("running handle.home");
-        fs.readFile(buildPath("index.html"), function(error,file){
-            if (error){
-                response.write(500, {"Content-type":"text/plain"});
-                response.end('server error');
-                console.log(error);
-                return;
-            }
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.end(file);
-        });
-    }
-}
-
-
+  home: function(request, response) {
+    console.log("running handle.home");
+    fs.readFile(buildPath("index.html"), function(error, file) {
+      if (error) {
+        response.writeHead(500, { "Content-type": "text/plain" });
+        response.end("server error");
+        console.log(error);
+        return;
+      }
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(file);
+    });
+  },
+  publicHandler: function(request, response) {
+      console.log("running publicHandler")
+    fs.readFile(buildPath(request.url), function(error, file) {
+      if (error) {
+        response.writeHead(500, { "Content-type": "text/plain" });
+        response.end("server error");
+        // console.log(error);
+        // return;
+      }
+      response.writeHead(200, {"Content-Type": mime.lookup(request.url)});
+      response.end(file);
+    });
+  }
+};
 
 module.exports = handler;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-const handler = require('./handlers')
+const handler = require("./handlers");
 
 const router = (req, res) => {
-    if(req.url == "/"){
-       handler.home(req,res);
-    }
-}
+  if (req.url == "/") {
+    handler.home(req, res);
+  } else {
+    handler.publicHandler(req, res);
+  }
+};
 
 module.exports = router;

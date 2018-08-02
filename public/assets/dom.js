ebab3c81155b077dfe7a53cde8fd26203e30f670
martingaston <squidman@gmail.com>
2018-08-02 16:41:27 +0100

    Add dom.js object work

--- public/assets/dom.js
+++ public/assets/dom.js
@@ -1,6 +1,22 @@
 const geoLoc = document.querySelector('.geoButton');
 const postcodeLoc = document.querySelector('.postcodeButton');
 const input = document.querySelector('#input_postcode');
+const display = document.querySelector('#display');
+
+const testObj = {
+  crimes: {
+    burglary: 300,
+    arson: 45,
+    'robbery crime': 33,
+    theft: 10,
+    theft: 54,
+  },
+  streets: {
+    'bad street 1': 220,
+    'Fonthill Road': 102,
+    'good street 12': 5,
+  },
+};
 
 geoLoc.addEventListener('click', (event) => {
   event.preventDefault();
@@ -8,9 +24,17 @@ geoLoc.addEventListener('click', (event) => {
   geoLocate((latitude, longitude) => {
     const locString = `/coords/?lat=${latitude}&long=${longitude}`;
     xhr('GET', locString, (geolocationData) => {
-      // ADD GEOLOCATION DISPLAY CODE HERE :D
       console.log(geolocationData);
     });
+    // TODO - put back in XHR for when it works
+    const p = document.createElement('p');
+    count = 0;
+    console.log(testObj.crimes.arson);
+    for (const x in testObj.crimes) {
+      console.log(testObj.crimes[x]);
+    }
+    p.textContent = `Oh no! There's been ${count} crimes in that location!`;
+    display.appendChild(p);
   });
 });
 

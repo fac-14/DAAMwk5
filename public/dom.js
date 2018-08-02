2d400fd275c61856807c1ddf69f6bf396725a546
martingaston <squidman@gmail.com>
2018-08-02 14:56:52 +0100

    add postcode button functionality Relates #7 and #8

--- public/dom.js
+++ public/dom.js
@@ -1,14 +1,27 @@
-var geoLoc = document.querySelector('.geoButton');
+const geoLoc = document.querySelector('.geoButton');
+const postcodeLoc = document.querySelector('.postcodeButton');
+const input = document.querySelector('#input_postcode');
 
-geoLoc.addEventListener('click', function(event) {
-event.preventDefault();    
-    console.log('button works?');
-    geoLocate(function(latitude, longitude) {
-        var locString = '/?lat=' + latitude + '&long=' + longitude;
-        xhr('GET', locString, function(data)
-    {
-        console.log(data);
-    })
-    })
-}
-);
+geoLoc.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('button works?');
+  geoLocate((latitude, longitude) => {
+    const locString = `/?lat=${latitude}&long=${longitude}`;
+    xhr('GET', locString, (geolocationData) => {
+      // ADD GEOLOCATION DISPLAY CODE HERE :D
+      console.log(geolocationData);
+    });
+  });
+});
+
+postcodeLoc.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('Postcode Search Button Pressed!');
+  const postcode = input.value;
+  const postcodeString = `/?postcode=${encodeURI(postcode.replace(' ', ''))}`;
+  console.log(postcodeString);
+  xhr('GET', postcodeString, (postcodeData) => {
+    // ADD POSTCODE FORMATTING/DISPLAY CODE HERE :D
+    console.log(postcodeData);
+  });
+});

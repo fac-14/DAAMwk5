7be58ea5262e0445597682c790c7459fa69474af
zeanort <armandwork@hotmail.co.uk>
2018-08-03 12:05:08 +0100

    debugging handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,56 +1,52 @@
-const fs = require("fs");
-const path = require("path");
-const mime = require("mime-types");
-const requestPkg = require("request");
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
+const requestPkg = require('request');
 
-const buildPath = function(myPath) {
-  return path.join(__dirname, "..", "public", myPath);
+const buildPath = function (myPath) {
+  return path.join(__dirname, '..', 'public', myPath);
 };
 
 const handler = {
   home(request, response) {
-    console.log("running handle.home");
-    fs.readFile(buildPath("index.html"), (error, file) => {
+    console.log('running handle.home');
+    fs.readFile(buildPath('index.html'), (error, file) => {
       if (error) {
-        response.writeHead(500, { "Content-type": "text/plain" });
-        response.end("server error");
+        response.writeHead(500, { 'Content-type': 'text/plain' });
+        response.end('server error');
         console.log(error);
         return;
       }
-      response.writeHead(200, { "Content-Type": "text/html" });
+      response.writeHead(200, { 'Content-Type': 'text/html' });
       response.end(file);
     });
   },
   publicHandler(request, response) {
     fs.readFile(buildPath(request.url), (error, file) => {
       if (error) {
-        response.writeHead(500, { "Content-type": "text/plain" });
-        response.end("server error");
+        response.writeHead(500, { 'Content-type': 'text/plain' });
+        response.end('server error');
         console.log(error);
         return;
       }
-      response.writeHead(200, { "Content-Type": mime.lookup(request.url) });
+      response.writeHead(200, { 'Content-Type': mime.lookup(request.url) });
       response.end(file);
     });
   },
   coordsHandler(request, response) {
-    const latlong = request.url.split("coords/")[1];
-    const lat = latlong.split("&long=")[0].split("=")[1];
-    const long = latlong.split("&long=")[1];
+    const latlong = request.url.split('coords/')[1];
+    const lat = latlong.split('&long=')[0].split('=')[1];
+    const long = latlong.split('&long=')[1];
     console.log(`lat ${lat} long ${long}`);
     handler.policeRequest(lat, long);
-    response.writeHead(302, { "Content-Type": "text/plain" });
+    response.writeHead(302, { 'Content-Type': 'text/plain' });
+    // response.end(handler.policeRequest(lat, long));
+    console.log('This is the object we need', handler.policeRequest(lat, long));
     response.end();
-
-    // NEXT STEPS:
-    // redirect user to home url,
-    // send request (using lat and long) to police API,
-    // get results from police
-    // send object of some sorts to client
   },
   postcodeHandler(request, response) {
     const decoded = decodeURI(request.url);
-    const postcode = decoded.split("postcode/")[1].replace(/[^A-Za-z0-9]/, "");
+    const postcode = decoded.split('postcode/')[1].replace(/[^A-Za-z0-9]/, '');
 
     // API request to get latitude and longitude from postcodes.io api
     requestPkg(
@@ -60,15 +56,15 @@ const handler = {
         const statusCode = response.statusCode;
         body = JSON.parse(body);
         console.log(
-          `lat: ${body.result.latitude}, long: ${body.result.longitude}`
+          `lat: ${body.result.latitude}, long: ${body.result.longitude}`,
         );
         handler.policeRequest(body.result.latitude, body.result.longitude);
-      }
+      },
     );
 
     // PLACEHOLDER
     console.log(`postcode is ${postcode}`);
-    response.writeHead(302, { "Content-Type": "text/plain" });
+    response.writeHead(302, { 'Content-Type': 'text/plain' });
     response.end();
 
     // NEXT STEPS:
@@ -80,33 +76,32 @@ const handler = {
   policeRequest(latitude, longitude) {
     requestPkg(
       `https://data.police.uk/api/crimes-street/all-crime?lat=${latitude}&lng=${longitude}`,
-      function(error, response, body) {
+      (error, response, body) => {
         console.log(`your error is: ${error}`);
         const policeData = JSON.parse(body);
+        // console.log(handler.responseCreator(policeData));
         handler.responseCreator(policeData);
-      }
+      },
     );
   },
   responseCreator(testArr) {
     // Object to send back to client
-    var responseObj = {
+    const responseObj = {
       crimes: {},
-      streets: {}
+      streets: {},
     };
 
     // Extract crimes and streets from the large JSON response. Make street descriptions more concise with regex
-    var listOfCrimes = testArr.map(i => i.category);
-    var listOfStreets = testArr.map(i =>
-      i.location.street.name.replace("On or near ", "")
-    );
+    const listOfCrimes = testArr.map(i => i.category);
+    const listOfStreets = testArr.map(i => i.location.street.name.replace('On or near ', ''));
 
     // Use callback to filter unique values from array
     function getUniqueValues(val, i, self) {
       return self.indexOf(val) === i;
     }
 
-    var uniqueCrimes = listOfCrimes.filter(getUniqueValues);
-    var uniqueStreets = listOfStreets.filter(getUniqueValues);
+    const uniqueCrimes = listOfCrimes.filter(getUniqueValues);
+    const uniqueStreets = listOfStreets.filter(getUniqueValues);
 
     // Initialise values in object (to avoid NaN issue)
     uniqueCrimes.forEach(i => (responseObj.crimes[i] = 0));
@@ -116,8 +111,8 @@ const handler = {
     listOfCrimes.forEach(i => (responseObj.crimes[i] += 1));
     listOfStreets.forEach(i => (responseObj.streets[i] += 1));
 
-    console.log(responseObj);
-  }
+    return responseObj;
+  },
 };
 
 module.exports = handler;
